b96e7b2281360cd29963ebe9c98434a54e5b0483
ehardarson <ellert.hardarson@gmail.com>
2017-03-16 13:03:49 +0100

    Added main() and structure of 3 lines with info.

--- zcreen.sh
+++ zcreen.sh
@@ -4,4 +4,20 @@
 #
 # Tool to print on screen stats in short.
 #
-echo "TODO"
+TRB=$(tput setaf 1 && tput bold)
+TRB=$(tput setaf 2 && tput bold)
+
+TOTALMEM=`grep MemTotal /proc/meminfo | awk '{$2=$2/(1024^2); split($2,a,".");  print a[1] " GB"}'`
+
+OUTPUT=``
+
+
+function main() {
+echo
+echo "$TOTALMEM"
+echo "LINE2"
+echo "LINE3"
+echo
+}
+
+main
