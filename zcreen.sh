9681a124208f4ebda5f2e4a980b2ef3208f94d57
ehardarson <ellert.hardarson@gmail.com>
2017-03-16 15:34:03 +0100

    Added total memory check,

--- zcreen.sh
+++ zcreen.sh
@@ -4,20 +4,26 @@
 #
 # Tool to print on screen stats in short.
 #
+#set -xv
+
+# Styling.
 TRB=$(tput setaf 1 && tput bold)
 TRB=$(tput setaf 2 && tput bold)
 
-TOTALMEM=`grep MemTotal /proc/meminfo | awk '{$2=$2/(1024^2); split($2,a,".");  print a[1] " GB"}'`
-
-OUTPUT=``
-
-
-function main() {
-echo
-echo "$TOTALMEM"
-echo "LINE2"
-echo "LINE3"
-echo
+function help () {
+  OUTPUT=``
 }
 
-main
+function mem() {
+TOTALMEM=`grep MemTotal /proc/meminfo | awk '{$2=$2/(1024^2); split($2,a,".");  print a[1] " GB"}'`
+#TOTALMEM=`grep MemTotal /proc/meminfo | awk '{$2=$2/1024; print $2}'`
+#RESULT=`awk -v n1=$TOTALMEM -v n2=0.0 'BEGIN{if (n1>n2) printf "BIG"; else printf "small";}'`
+}
+
+mem
+
+printf "\n"
+printf "Totoal Memory : $TOTALMEM\n"
+#printf "Result : $RESULT\n"
+#printf "LINE3\n"
+printf "\n"
