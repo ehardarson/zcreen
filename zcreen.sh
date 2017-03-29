737fda2f59c84de7eee96a7ed7c6db9d2c6ec95c
ehardarson <ellert.hardarson@gmail.com>
2017-03-29 14:25:54 +0200

    Added MemTotal, MemFree and MemAvailable

--- zcreen.sh
+++ zcreen.sh
@@ -6,6 +6,9 @@
 #
 #set -xv
 
+BASEDIR=$(dirname "$0")
+echo "$BASEDIR"
+
 # Styling.
 TRB=$(tput setaf 1 && tput bold)
 TRB=$(tput setaf 2 && tput bold)
@@ -15,15 +18,21 @@ function help () {
 }
 
 function mem() {
-TOTALMEM=`grep MemTotal /proc/meminfo | awk '{$2=$2/(1024^2); split($2,a,".");  print a[1] " GB"}'`
+#TOTALMEM=`grep MemTotal /proc/meminfo | awk '{$2=$2/(1024^2); split($2,a,".");  print a[1] " GB"}'`
 #TOTALMEM=`grep MemTotal /proc/meminfo | awk '{$2=$2/1024; print $2}'`
 #RESULT=`awk -v n1=$TOTALMEM -v n2=0.0 'BEGIN{if (n1>n2) printf "BIG"; else printf "small";}'`
+MemTotal=`grep MemTotal /proc/meminfo | awk '{ print $2}'`
+MemFree=`grep MemFree /proc/meminfo | awk '{ print $2}'`
+MemAvailable=`grep MemAvailable /proc/meminfo | awk '{print $2}'`
+
+MemPercentageUsed=
+
 }
 
 mem
 
 printf "\n"
-printf "Totoal Memory : $TOTALMEM\n"
+printf "Totoal Memory : $MemTotal  /  MemFree : $MemFree  /  MemAvailable : $MemAvailable\n"
 #printf "Result : $RESULT\n"
 #printf "LINE3\n"
 printf "\n"
